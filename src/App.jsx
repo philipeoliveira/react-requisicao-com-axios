b36055092d69a57e281e6560bc7ab40d93068a65
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-10-31 18:00:15 -0300

    Modify page layout

--- src/App.jsx
+++ src/App.jsx
@@ -32,38 +32,51 @@ export default function LastDraw() {
    }, []);
 
    return (
-      <section>
-         <h2>Último sorteio da Lotofácil</h2>
-
-         {isLoading ? (
-            <p>Carregando...</p>
-         ) : error ? (
-            <div>
-               <p>{error.message}</p>
-               <p>Não foi possível conectar-se à API da Loterias Caixa.</p>
-            </div>
-         ) : (
-            data && (
-               <div key={data.numero}>
-                  <p>Data do sorteio: {data.dataApuracao}</p>
-                  <div>
-                     <p>
-                        Concurso: <span>{data.numero}</span>
-                     </p>
-                     <p>
-                        {ascOrder
-                           ? data.listaDezenas.join('-')
-                           : data.dezenasSorteadasOrdemSorteio.join('-')}
-                     </p>
-                     <p>
-                        <button onClick={toggleOrder}>
-                           {ascOrder ? 'Ordem de sorteio' : 'Ordem crescente'}
-                        </button>
-                     </p>
-                  </div>
+      <div>
+         <main>
+            <h1>Sorteio mais recente da Lotofácil</h1>
+            {isLoading ? (
+               <p>Carregando...</p>
+            ) : error ? (
+               <div>
+                  <p>{error.message}</p>
+                  <p>Não foi possível conectar-se à API da Loterias Caixa.</p>
                </div>
-            )
-         )}
-      </section>
+            ) : (
+               data && (
+                  <div key={data.numero}>
+                     <p>Data do sorteio: {data.dataApuracao}</p>
+                     <div>
+                        <p>
+                           Concurso: <b>{data.numero}</b>
+                        </p>
+                        <h2>
+                           {ascOrder
+                              ? data.listaDezenas.join('-')
+                              : data.dezenasSorteadasOrdemSorteio.join('-')}
+                        </h2>
+                        <p>
+                           <button onClick={toggleOrder}>
+                              {ascOrder ? 'Ordem de sorteio' : 'Ordem crescente'}
+                           </button>
+                        </p>
+                     </div>
+                  </div>
+               )
+            )}
+         </main>
+         <footer>
+            <p>
+               Desenvolvido por{' '}
+               <a
+                  href='https://github.com/philipeoliveira'
+                  title='Abrir em nova aba o GitHub do autor Philipe Oliveira'
+                  target='_blank'
+               >
+                  Philipe Oliveira
+               </a>
+            </p>
+         </footer>
+      </div>
    );
 }
